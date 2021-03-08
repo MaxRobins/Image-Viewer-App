39623d0bc792067934e698a6fddc09192a06481e
Maxwell Young Robins <tug78217@temple.edu>
2021-03-07 20:50:38 -0500

    assignment 4 finished

--- app/src/main/java/edu/temple/dogimageviewerapp/SelectionActivity.java
+++ app/src/main/java/edu/temple/dogimageviewerapp/SelectionActivity.java
@@ -2,6 +2,7 @@ package edu.temple.dogimageviewerapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,24 +17,34 @@ public class SelectionActivity extends AppCompatActivity {
 
     ImageView imageView;
     Spinner spinner;
-    //String defaultTextForSpinner = "Select a Breed";
-
     int[] dogImagesArray;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().setTitle("Selection Activity");
+
         spinner =findViewById(R.id.spinner);
         imageView = findViewById(R.id.imageView);
 
+
         ArrayList breedArray = new ArrayList<String>();
+        breedArray.add(0,"Select item from list");
         breedArray.add("BlackLab");
         breedArray.add("Bulldog");
         breedArray.add("Bullmastiff");
         breedArray.add("Rottweiler");
 
-        dogImagesArray = new int[]{R.drawable.blacklab, R.drawable.bulldog, R.drawable.bullmastiff, R.drawable.rottweiler};
+
+        ArrayList colorArray = new ArrayList<String>();
+        colorArray.add(0,"");
+        colorArray.add("Black");
+        colorArray.add("White");
+        colorArray.add("Brown");
+        colorArray.add("Black");
+
+        dogImagesArray = new int[]{android.R.color.transparent,R.drawable.blacklab, R.drawable.bulldog, R.drawable.bullmastiff, R.drawable.rottweiler};
 
         ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, breedArray);
 
@@ -42,9 +53,15 @@ public class SelectionActivity extends AppCompatActivity {
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                //showPicture(position);
-                Toast.makeText(SelectionActivity.this, "Item Selected", Toast.LENGTH_SHORT).show();
-                showPicture(position);
+                if(parent.getItemAtPosition(position).equals("Select item from list")){
+                    imageView.setImageResource(android.R.color.transparent);
+                }
+                else{
+                    Intent launchIntent = new Intent(SelectionActivity.this, DisplayActivity.class);
+                    launchIntent.putExtra("name", spinner.getSelectedItem().toString());
+                    launchIntent.putExtra("image", dogImagesArray[position]);
+                    startActivity(launchIntent);
+                }
             }
 
             @Override
@@ -53,7 +70,13 @@ public class SelectionActivity extends AppCompatActivity {
             }
         });
     }
-    private void showPicture(int position){
-        imageView.setImageResource(dogImagesArray[position]);
-    }
+            @Override
+            protected void onResume(){
+                super.onResume();;
+                spinner.setSelection(0);
+            }
+
+            private void showPicture(int position){
+                imageView.setImageResource(dogImagesArray[position]);
+            }
 }
